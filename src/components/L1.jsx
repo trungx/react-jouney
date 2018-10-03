483299d1deaa66ffd57f78914d3f6081b5016f03
Trung Hong Ta <trung.ta@hdwebsoft.co>
2018-10-03 17:11:07 +0700

    inital project jouney leaning curve

---
+++ src/components/L1.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class L1 extends Component {
+    render() {
+        return (
+            <div className="L1">
+              <h1>Hello L1</h1>
+            </div>
+        );
+    }
+}
+
+export default L1;
